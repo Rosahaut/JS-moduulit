a9d685f48e1479da2128ac37c9be2d76406d6926
Rosmu <rosamari.sofia@gmail.com>
2024-04-11 21:37:28 +0300

    js-moduuli01 tehtävien korjailua

--- moduuli01/assignment10/task10.js
+++ moduuli01/assignment10/task10.js
@@ -1,20 +1,21 @@
 'use strict';
 
-    const diceCount = parseInt(prompt('Enter the number of dice:'));
-    const targetSum = parseInt(prompt('Enter the target sum:'));
+const diceCount = parseInt(prompt('Enter the number of dice:'));
+const targetSum = parseInt(prompt('Enter the target sum:'));
 
-    let successRate = 0;
-    let replays = 10000;
-    for (let i = 0; i < replays; i++) {
-        let sum = 0;
-        for (let j = 0; j < diceCount; j++) {
-            let rand = Math.floor(Math.random() * 6) + 1;
-            sum += rand;
-        }
-        if (sum === targetSum) successRate++;
+let successRate = 0;
+let replays = 10000;
+for (let i = 0; i < replays; i++) {
+  let sum = 0;
+  for (let j = 0; j < diceCount; j++) {
+    let rand = Math.floor(Math.random() * 6) + 1;
+    sum += rand;
+  }
+  if (sum === targetSum) successRate++;
+}
 
-    }
+const odds = successRate / replays * 100;
 
-    const odds = successRate / replays * 100;
-
-    document.querySelector('#target').innerHTML = `Probability to get sum ${targetSum} with ${diceCount} dice is ${odds.toFixed(2)}%.`;
+document.querySelector(
+    '#target').innerHTML = `Probability to get sum ${targetSum} with ${diceCount} dice is ${odds.toFixed(
+    2)}%.`;
